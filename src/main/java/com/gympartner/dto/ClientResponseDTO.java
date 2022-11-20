2d52bd58aee6e7d7276032daf0556d9d5a07139d
Christhony <christhony2002@gmail.com>
2022-11-19 21:40:12 -0500

    ClientDTO

--- src/main/java/com/gympartner/dto/ClientResponseDTO.java
+++ src/main/java/com/gympartner/dto/ClientResponseDTO.java
@@ -1,4 +1,26 @@
 package com.gympartner.dto;
 
+import com.gympartner.entities.Coach;
+import com.gympartner.entities.Gym;
+import lombok.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientResponseDTO {
+    private Long id;
+    private String name;
+    private String lastName;
+    private Gym gym;
+    private String email;
+    private String personalGoal;
+    private LocalDate birthday;
+    private String physicalState;
+    private Integer tall;
+    private Integer weight;
+    private Coach coach;
+    private String phone;
 }
